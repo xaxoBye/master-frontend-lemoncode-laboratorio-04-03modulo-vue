2a3ef1e922f3e172b7954ba2100204cd15456f38
jpujala <jpujala@stp.es>
2026-04-30 20:40:39 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <h1>Planificador de menús semanal</h1>
+    <button>Planificador</button>
+    <button>Favoritos</button>
+    <button>configuración</button>
+  </header>
+</template>
+
